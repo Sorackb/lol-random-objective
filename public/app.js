7b446ad1271cb5ad10cbf128733e469ca2958e9b
Lucas Bernardo <ti.desenvolvimento@madalozzocorretora.com.br>
2018-11-16 16:24:26 -0200

    list

--- public/app.js
+++ public/app.js
@@ -11,8 +11,7 @@ function ObjectiveController() {
   const vm = this;
 
   const gerar = () => {
-    vm.selected = vm.objectives[Math.floor(Math.random() * vm.objectives.length)];
-    console.log(vm.selected);
+    vm.selected = Math.floor(Math.random() * vm.objectives.length);
   }
 
   const init = () => {
